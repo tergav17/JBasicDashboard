42feefc7e651e86bf6c2601002b871e7a23d18dc
tergav17 <gctersteeg@gmail.com>
2020-12-02 23:48:19 -0600

    Added code for the JBHorizontalSlider

---
+++ src/dash/JBHorizontalSlider.java
@@ -0,0 +1,11 @@
+package dash;
+
+public class JBHorizontalLine {
+    private int x;
+    private int y;
+    private int width;
+
+    private double realPos;
+    private double max;
+    private double min;
+}
