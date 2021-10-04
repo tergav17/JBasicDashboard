93188a74e2f449fbfdc6e4c2e71acf81be0d12c4
tergav17 <gctersteeg@gmail.com>
2021-10-04 18:51:08 -0500

    Removed Toolkit stuff

--- src/dash/ResourceLoader.java
+++ src/dash/ResourceLoader.java
@@ -1,12 +1,47 @@
 package dash;
 
-import java.awt.Image;
-import java.awt.Toolkit;
+import sun.awt.image.ToolkitImage;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class ResourceLoader {
 	private static ResourceLoader rl = new ResourceLoader();
 	
 	public static Image getImage(String fileName) {
-		return Toolkit.getDefaultToolkit().getImage(rl.getClass().getResource(fileName));
+		Image i = null;
+		try {
+			i = ImageIO.read(rl.getClass().getResource(fileName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		BufferedImage b = toBufferedImage(i);
+
+		if (b == null) System.err.println("IMAGE LOAD FAILURE: " + fileName);
+
+		return b;
+	}
+
+	//Waits for an image to load, then transfers it to a buffered image
+	public static BufferedImage toBufferedImage(Image img) {
+		try {
+			MediaTracker mt = new MediaTracker(new JPanel());
+			mt.addImage(img, 0);
+			mt.waitForAll();
+
+			BufferedImage bi = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+			Graphics2D g2d = bi.createGraphics();
+			g2d.drawImage(img, 0, 0, null);
+			g2d.dispose();
+
+			return bi;
+		} catch (InterruptedException ex) {
+		}
+
+		return null;
 	}
 }
